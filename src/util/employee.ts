a083086238b8c52334ec917631fb2dc32dd298a8
Jatin Raghav <raghavjatin123@gmail.com>
2023-02-03 20:58:25 +0530

    remove keys

--- src/util/employee.ts
+++ src/util/employee.ts
@@ -1,11 +1,11 @@
-// const fs = require("fs");
+import { Employee } from "src/type/employee";
 import employeeJson from "../config/employee.json";
 import { SendgridMail } from "./sendgridMail";
 export class CalculateEmployeeRecords {
-  public async calculateEmployeeData(): Promise<any> {
+  public async calculateEmployeeData(): Promise<void> {
     try {
       const sendgridMail = new SendgridMail();
-      const data = employeeJson as any;
+      const data = employeeJson as Employee;
       const employeesWithOneOrNoAttendance = data.employees.filter(
         (employee: any) => employee.attendance.length <= 1
       );
@@ -13,11 +13,9 @@ export class CalculateEmployeeRecords {
         (employee: any) => employee.email
       );
 
-      sendgridMail.sendEmail(
-        employeeEmails.join(","),
-        {},
-        "d-babef0e5f86c42d2a1159e37810bc04a"
-      );
+      employeeEmails.forEach((email: string) => {
+        sendgridMail.sendEmail(email, {}, "");
+      });
 
       console.log(employeeEmails);
     } catch (error) {
